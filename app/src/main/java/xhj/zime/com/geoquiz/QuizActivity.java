1ca1ab58a35060e410a7fa9fbc38c4edb18bcfb6
谢浩杰 <1012351395@qq.com>
2018-08-30 09:50:09 +0800

    此项目需改进的地方:1.禁止一题多答;2.用户答完所有题目之后显示一个toast显示百分比形式的评分;3.解决漏洞:a.用户作弊后,可以旋转CheatActivity来清除作弊痕迹;b.作弊返回后,用户可以旋转QuizActivity来清除mIsCheated变量值;c.用户可以不断点击NEXT按钮,跳到偷看过答案的问题,从而使作弊记录丢失;4.允许用户作弊,但是限制作弊次数,在CHEAT按钮下显示剩余次数,超出后禁止偷看答案;

---
+++ app/src/main/java/xhj/zime/com/geoquiz/QuizActivity.java
@@ -0,0 +1,132 @@
+package xhj.zime.com.geoquiz;
+
+import android.content.Intent;
+import android.net.rtp.RtpStream;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+    private static final String KEY_INDEX = "index";
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mCheatButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+    private TextView mQuestionTextView;
+    private static final int REQUEST_CODE_CHEAT = 0;
+    private boolean mIsCheated;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_text1, true),
+            new Question(R.string.question_text2, false),
+            new Question(R.string.question_text3, true),
+            new Question(R.string.question_text4, false),
+            new Question(R.string.question_text5, true)
+    };
+
+    private int mCurrentIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheated = false;
+                updateQuestion();
+            }
+        });
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 4) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        updateQuestion();
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(false);
+            }
+        });
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivityForResult(intent, REQUEST_CODE_CHEAT);
+            }
+        });
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageId = 0;
+        if (mIsCheated) {
+            messageId = R.string.judgemet_toast;
+        } else {
+            if (userPressedTrue == answerIsTrue) {
+                messageId = R.string.correct_toast;
+            } else {
+                messageId = R.string.incorrect_toast;
+            }
+        }
+
+        Toast.makeText(this, messageId, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+            mIsCheated = CheatActivity.wasAnswerShown(data);
+        }
+    }
+}
